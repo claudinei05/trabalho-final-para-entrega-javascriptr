b28696699019688c549f06c66bd48bb001520a92
Claudinei Braga de Oliveira <oliveiraclaudinei786@gmail.com>
2022-09-20 08:01:41 -0300

    1

--- javascript/criarConta.js
+++ javascript/criarConta.js
@@ -1,30 +1,88 @@
 class Validator {
   constructor() {
-    this.validations = [];
+    this.validations = ["data-min-length"];
+  }
+  //iniciar validação de todos os campos
+  validate(form) {
+    //pegar os inputs
+    let inputs = form.getElementsByTagName("input");
+
+    //htmlcollection -> array
+    let inputsArray = [...inputs];
+
+    //lopp nos inputs e valdação mediante ao que for encontrado
+    inputsArray.forEach(function (input) {
+      //loop de todas as validação existente
+      for (let i = 0; this.validations.length > i; i++) {
+        //verifica se a validação atual existe no input
+        if (input.getAttribute(this.validations[i]) != null) {
+          //1 data-min-length->minilength
+          //2 limpa a estring para virar um metodo
+          let method = this.validations[i]
+            .replace("data-", "")
+            .replace("-", "");
+
+          //valor do input
+          let value = input.getAttribute(this.validations[i]);
+
+          //invocar o metodo
+          this[method](input, value);
+        }
+      }
+    }, this);
+  }
+
+  //verifica se um input tem um numero minimo de caracteres
+  minlength(input, minValue) {
+    let inputLength = input.value.length;
+
+    let erroMensage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+
+    if (inputLength < minValue) {
+      console.log(erroMensage);
+      this.printMessage(input, erroMensage);
+    }
+  }
+
+  //metodo vai imprimir mensagen de erro na tela
+  printMessage(input, msg) {
+    let template = document.querySelector(".error-validation").cloneNode(true);
+    template.textContent = msg;
+
+    let inputParent = input.parentNode;
+    template.classList.remove("template");
+
+    inputParent.appendChild(template);
   }
 }
-
-//iniciar validação de todos os campos
-// validate(form) {
-//   //pegar os inputs
-//   let inputs = form.getElementsByTagName("input");
-
-//   console.log(inputs);
-
-//   //htmlcollection -> array
-//   let inputsArray = [...inputs];
-
-//   console.log(inputsArray);
-// }
 
 let form = document.getElementById("register-form");
 let submit = document.getElementById("submit");
 
 let validator = new Validator();
 
-// evento que dispara as validações
+// // evento que dispara as validações
 submit.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("funcionou");
   validator.validate(form);
 });
+
+// let user = [
+//   {
+//       username: 'adriano',
+//       password: '123',
+//       messages: []
+//   }
+
+// ]
+
+// let newMessage =
+// {
+//   description: 'First',
+//   detail: 'last'
+// }
+
+// user[0].messages.push(newMessage)
+
+// console.log(user[0].messages)
--- javascript/listaRecados.js
+++ javascript/listaRecados.js
@@ -1,17 +1,37 @@
-let recados = [];
+// let recados = [];
 
-const formCadRecados = document.getElementById("formRecados-1");
-const descricao = document.getElementById("descriçao");
-const detalhamento = document.getElementById("detalhamento");
+// const formCadRecados = document.getElementById("formRecados-1");
+// const descricao = document.getElementById("descriçao");
+// const detalhamento = document.getElementById("detalhamento");
 
-formCadRecados.addEventListener("submit", (e) => {
-  e.preventDefault();
+// formCadRecados.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  const recado = {
-    id: Math.floor(Date.now() / 1000),
-    name: descricao.value,
-    type: Text.value,
-  };
-  recado.push(recado);
-  console.log(recado);
-});
+//   const recado = {
+//     id: Math.floor(Date.now() / 1000),
+//     name: descricao.value,
+//     type: Text.value,
+//   };
+//   recado.push(recado);
+//   console.log(recado);
+// });
+
+function recados(num, descricao, detalhamento, acao) {
+  let tb = document.getElementById("tableBody");
+  let qtdlinhas = tb.rows.length;
+  let linhas = tb.insertRow(qtdlinhas);
+
+  let cellcodigo = linhas.insertCell(0);
+  let celldescricao = linhas.insertCell(1);
+  let celldetalhamento = linhas.insertCell(2);
+  let cellacao = linhas.insertCell(3);
+
+  cellcodigo.innerHTML = qtdlinhas;
+  linhas.innerHTML = num;
+  celldescricao.innerHTML = descricao;
+  celldetalhamento.innerHTML = detalhamento;
+  cellacao.innerHTML = acao;
+  cellcodigo.innerHTML = qtdlinhas;
+  cellcodigo.innerHTML = qtdlinhas;
+  cellcodigo.innerHTML = qtdlinhas;
+}
